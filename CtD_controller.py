5573e5f31ea7a30ee4edbedf32a96053ead6e702
JulietteVl <52038457+JulietteVl@users.noreply.github.com>
2020-10-20 14:29:02 +0200

    Update CtD_controller.py
    Template du controller.

--- CtD_controller.py
+++ CtD_controller.py
@@ -1,7 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 20 14:20:18 2020
+from PyQt5.QtCore import *
+from model import *
 
-@author: julie
-"""
+class BaseController:
+    def __init__(self):
+        self.clients = list()
+        self.message = ""
 
+    def add_client(self, client):
+        self.clients.append(client)
+
+    def refresh_all(self, message):
+        self.message = message
+        for client in self.clients:
+            client.refresh()
+
+class CtDcontroller(BaseController):
+    def __init__(self):
+        super().__init__()
