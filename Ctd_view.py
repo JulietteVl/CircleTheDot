b3821caac23fe6ce8781368758a6f2c115ad8dd8
JulietteVl <52038457+JulietteVl@users.noreply.github.com>
2020-10-20 14:22:55 +0200

    Update and rename view.py to Ctd_view.py

--- Ctd_view.py
+++ Ctd_view.py
@@ -9,7 +9,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-from controler import *
+from controller import *
 
 class GameScene(QGraphicsScene):
         def __init__(self, parent, controler):
