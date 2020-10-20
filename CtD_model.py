aaacab0f03491eac0c30cc3049ab537b6bc783c8
Ke-20 <73174349+Ke-20@users.noreply.github.com>
2020-10-20 17:08:47 +0200

    Update CtD_model.py

--- CtD_model.py
+++ CtD_model.py
@@ -5,3 +5,34 @@ Created on Tue Oct 20 14:07:37 2020
 @author: julie
 """
 
+import random
+
+class Fuyard:
+    def __init__(self,x:int,y:int):
+        self.x = x
+        self.y = y
+    
+    def move(self,w,h,l_cond):
+        pass
+    
+    def __repr__(self):
+        return f"Le fuyard est en (x={self.x:2d} ; y={self.y:2d})"
+    
+    
+
+
+class Board:
+    def __init__(self,w=10,h=10,nb_cond=6,fw=3,fh=3):
+        self.width = w
+        self.height = h
+        self.l_cond = []
+        self.nb_cond = nb_cond
+        self.fuy_width = fw
+        self.fuy_height = fh
+        self.fuyard = Fuyard(int(self.width/2)+random.randint(-self.fuy_width/2,self.fuy_width/2),int(self.height/2)+random.randint(-self.fuy_height/2,self.fuy_height/2))
+        
+        
+    def cond(self,x,y):
+        pos = x,y
+        if pos not in self.l_cond:
+            self.l_cond.append(pos)
