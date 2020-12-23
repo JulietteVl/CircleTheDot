2a44b84d32fe0394046e802d6636f0592dfbe315
Ke-20 <73174349+Ke-20@users.noreply.github.com>
2020-12-23 16:53:07 +0100

    Update IA et écrans

--- CtD_model.py
+++ CtD_model.py
@@ -31,6 +31,105 @@ class Fugitive:
             self.y = pos[1]
             return("escaping")
     
+    def move_moy(self, board, level = 0 ):
+        paths = []
+        current_path = []
+        
+        for p in range(250):
+            state = "escaping"
+            current_path.clear()
+            cx = self.x
+            cy = self.y
+            while state == "escaping" and len(current_path) < 20:
+                l_cond = board.l_cond
+                if level == 0:
+                    choices = [(cx,cy+1),   #above
+                               (cx,cy-1),   #below
+                               (cx+1,cy),
+                               (cx-1,cy)]
+                    if cx%2 == 0:
+                        choices.append((cx+1,cy-1))
+                        choices.append((cx-1,cy-1))
+                    else:
+                        choices.append((cx+1,cy+1))
+                        choices.append((cx-1,cy+1))
+                    for c in choices.copy():
+                        if (c in board.l_cond):
+                            choices.remove(c)
+                        if (c in current_path):
+                            choices.remove(c)
+                    if len(choices) == 0:
+                        state = "stuck"
+                    else :
+                        pos = random.choice(choices)
+                        if (pos[0]<0) or (pos[1]<0) or (pos[0]>=board.width) or (pos[1]>=board.height):
+                            state = "free"
+                        else :
+                            state = "escaping"
+                        cx = pos[0]
+                        cy = pos[1]
+                        cp = cx,cy
+                        current_path.append(cp)
+            if state == "free":                
+                paths.append(current_path)
+        
+        if not paths:
+            ret = self.move(board)
+            return(ret)
+        else:
+            paths.sort(key=len)
+            best = paths[0]
+            cx,cy = best[0]
+            if (cx<0) or (cy<0) or (cx>=board.width) or (cy>=board.height):
+                return("free")
+            self.x,self.y = best[0]
+                    
+    def move_hard(self, board, level = 0):
+        if (self.x<1) or (self.y<1) or (self.x>=board.width-1) or (self.y>=board.height-1):
+           return("free")
+        paths = []
+        cp = []
+        t = self.x,self.y
+        cp.append(t)
+        paths.append(cp)
+        state = "escaping"
+        while state == "escaping":
+            for p in paths.copy():
+                ct = p[len(p)-1]
+                cx,cy = ct
+                if level == 0:
+                    choices = [(cx,cy+1),   #above
+                               (cx,cy-1),   #below
+                               (cx+1,cy),
+                               (cx-1,cy)]
+                    if cx%2 == 0:
+                        choices.append((cx+1,cy-1))
+                        choices.append((cx-1,cy-1))
+                    else:
+                        choices.append((cx+1,cy+1))
+                        choices.append((cx-1,cy+1))
+                    for c in choices.copy():
+                        if (c in board.l_cond):
+                            choices.remove(c)
+                        elif (c in p):
+                            choices.remove(c)
+                    if not choices:
+                        ret = self.move(board)
+                        return(ret)
+                    else:
+                        paths.remove(p)
+                        for c in choices :
+                            if (c[0]<0) or (c[1]<0) or (c[0]>=board.width) or (c[1]>=board.height):
+                                state = "free"
+                                temp = p[1]
+                                self.x,self.y = temp
+                            else:
+                                p.append(c)
+                                paths.append(p.copy())
+                                p.remove(c)
+        return("escaping")                      
+        
+    
     def __repr__(self):
         return f"The fugitive is at (x={self.x:2d} ; y={self.y:2d})"
     
