6e9606d3b7243c91e8152c8b3c121a09c8262476
JulietteVl <52038457+JulietteVl@users.noreply.github.com>
2021-05-01 00:15:14 +0200

    Resolve PEP8 issues and add doctring (#4)
    Merge pull request #4 from #3 
    
    * Resolve PEP8 issues
    Excluding some text lines that are still too long.
    
    * Added docstrings to modules, classes and functions

--- CtD_model.py
+++ CtD_model.py
@@ -1,125 +1,193 @@
+"""
+The model manages the data, logic and rules of the application.
+
+Classes:
+    Fugitive
+    Board
+"""
+
 import random
 from math import sqrt
 
+
 class Fugitive:
-    # This is incarnated by the red circle trying to escape the board.
-    def __init__(self,x:int,y:int):
+    """Incarnated by the red circle trying to escape the board."""
+
+    def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
-    
-    def get_choices(self, board, cx, cy):
-        # Give the coordinates of the 6 adjacent cases, excluding the condemned ones.
-        choices = [(cx,cy+1),   #above
-                   (cx,cy-1),   #below
-                   (cx+1,cy),
-                   (cx-1,cy)]
-        if cx%2 == 0: # this dependency is due to the grid being hexagonal.
-            choices.append((cx+1,cy-1))
-            choices.append((cx-1,cy-1))
+
+    def get_choices(self, board, cx: int, cy: int):
+        """
+        Give the coordinates of cells that can be accessed by the fugitive.
+
+        Parameters
+        ----------
+        board : Board
+            The board used in the game
+        cx : int
+            Coordinate along the x (horizontal) axis
+        cy : int
+            Coordinate along the y (vertical) axis
+
+        Returns
+        -------
+        choices : array
+            array containing tuples of coordinates
+
+        """
+        # Give the coordinates of the 6 adjacent cases, excluding the condemned
+        # ones.
+        choices = [(cx, cy+1),   # above
+                   (cx, cy-1),   # below
+                   (cx+1, cy),
+                   (cx-1, cy)]
+        if cx % 2 == 0:   # this dependency is due to the grid being hexagonal.
+            choices.append((cx+1, cy-1))
+            choices.append((cx-1, cy-1))
         else:
-            choices.append((cx+1,cy+1))
-            choices.append((cx-1,cy+1))
-            
+            choices.append((cx+1, cy+1))
+            choices.append((cx-1, cy+1))
+
         for c in choices.copy():
             if (c in board.l_cond):
                 choices.remove(c)
-        
+
         return choices
-    
+
     def move(self, board):
-        # At the minimum level, the choice is random. The fugitive is blind.
-        choices = self.get_choices(board, self.x,self.y)
+        """Randomly change fugitive position."""
+        choices = self.get_choices(board, self.x, self.y)
         if len(choices) == 0:
-            return("stuck 0") # 0 is the level.
-        
+            return("stuck 0")   # 0 is the level.
+
         pos = random.choice(choices)
-        if (pos[0]<0) or (pos[1]<0) or (pos[0]>=board.width) or (pos[1]>=board.height): 
-            return("free 0") # condition for leaving the board is met
+        if (
+                (pos[0] < 0)
+                or (pos[1] < 0)
+                or (pos[0] >= board.width)
+                or (pos[1] >= board.height)
+                ):
+            return("free 0")    # condition for leaving the board is met
         self.x = pos[0]
         self.y = pos[1]
         return("escaping 0")
-    
+
     def move_moy(self, board):
-        # The fugitive makes several random guesses and make the first step of the shortest.
+        """
+        Change fugitive position.
+
+        The fugitive makes several random guesses and make the first step of
+        the shortest.
+        """
         paths = []
-        
-        for p in range(board.height*board.width//10): # The number of guesses is arbitrary
+
+        for p in range(board.height*board.width//10):
+            # The number of guesses is arbitrary
             state = "escaping"
             current_path = []
             cx = self.x
             cy = self.y
-            while state == "escaping" and len(current_path) < 2*sqrt(board.height*board.width): # The length is arbitrary.
-                choices = self.get_choices(board, cx,cy)
+            while (state == "escaping"
+                   and len(current_path) < 2*sqrt(board.height*board.width)):
+                # The length is arbitrary.
+                choices = self.get_choices(board, cx, cy)
                 for c in choices.copy():
                     if (c in current_path):
                         choices.remove(c)
                 if len(choices) == 0:
                     state = "stuck"
-                else :
+                else:
                     pos = random.choice(choices)
-                    if (pos[0]<0) or (pos[1]<0) or (pos[0]>=board.width) or (pos[1]>=board.height):
+                    if (
+                            (pos[0] < 0)
+                            or (pos[1] < 0)
+                            or (pos[0] >= board.width)
+                            or (pos[1] >= board.height)
+                            ):
                         state = "free"
-                    else :
+                    else:
                         state = "escaping"
                     cx = pos[0]
                     cy = pos[1]
-                    cp = cx,cy
+                    cp = cx, cy
                     current_path.append(cp)
-                    
-            if state == "free":                
+
+            if state == "free":
                 paths.append(current_path)
-                
+
         if not paths:
             return(self.move(board))
         else:
             paths.sort(key=len)
             best = paths[0]
-            cx,cy = best[0]
-            if (cx<0) or (cy<0) or (cx>=board.width) or (cy>=board.height):
+            cx, cy = best[0]
+            if (
+                    (cx < 0)
+                    or (cy < 0)
+                    or (cx >= board.width)
+                    or (cy >= board.height)
+                    ):
                 return("free 1")
-            self.x,self.y = best[0]
+            self.x, self.y = best[0]
             return("escaping 1")
-                    
+
     def move_hard(self, board):
-        # The fugitive considers all the possible path and make the first step to the shortest.
-        if (self.x<1) or (self.y<1) or (self.x>=board.width-1) or (self.y>=board.height-1):
-           return("free 2")
+        """Change fugitive position.
+
+        The fugitive considers all the possible path and make the first step
+        to the shortest.
+        """
+        if (
+                (self.x < 1)
+                or (self.y < 1)
+                or (self.x >= board.width-1)
+                or (self.y >= board.height-1)
+                ):
+            return("free 2")
         paths = []
         cp = []
-        t = self.x,self.y
+        t = self.x, self.y
         cp.append(t)
         paths.append(cp)
         state = "escaping"
         while state == "escaping":
             for p in paths.copy():
                 ct = p[len(p)-1]
-                cx,cy = ct
-                choices = self.get_choices(board,cx,cy)
+                cx, cy = ct
+                choices = self.get_choices(board, cx, cy)
                 for c in choices.copy():
                     if (c in p):
                         choices.remove(c)
                 paths.remove(p)
-                if choices: #  Is there anywhere we can go from this cell?
+                if choices:     # Is there anywhere we can go from this cell?
                     for c in choices:
-                        if (c[0]<0) or (c[1]<0) or (c[0]>=board.width) or (c[1]>=board.height):
-                            self.x,self.y = p[1]
+                        if (
+                                (c[0] < 0)
+                                or (c[1] < 0)
+                                or (c[0] >= board.width)
+                                or (c[1] >= board.height)
+                                ):
+                            self.x, self.y = p[1]
                             return("escaping 2")
-                        else: # grow the paths
+                        else:   # grow the paths
                             p.append(c)
                             paths.append(p.copy())
                             p.remove(c)
-                elif paths == []: # No path lead to the exit. The fugitive choose a random cell.
+                elif paths == []:
+                    # No path lead to the exit. The fugitive choose a random
+                    # cell.
                     return(self.move(board))
-        return("escaping 2")                      
-        
-    
+        return("escaping 2")
+
     def __repr__(self):
+        """Return representation of a fugitive object."""
         return f"The fugitive is at (x={self.x:2d}; y={self.y:2d})"
-    
-    
 
 
 class Board:
+    """Game board and its evolution."""
+
     def __init__(self, w, h, nb_cond, fw, fh):
         self.width = w
         self.height = h
@@ -127,42 +195,47 @@ class Board:
         self.l_cond = []
         self.fuy_width = fw
         self.fuy_height = fh
-        
+
         # Avoid being outside of the grid:
-        self.fugitive = Fugitive((random.randint(max(int(self.width/2-self.fuy_width/2),1), 
-                                                 min(int(self.width/2+self.fuy_width/2),self.width-2))),
-                                 (random.randint(max(int(self.height/2-self.fuy_height/2),1),
-                                                 min(int(self.height/2+self.fuy_height/2),self.height-2))))
+        self.fugitive = Fugitive(
+            (random.randint(max(int(self.width/2-self.fuy_width/2), 1),
+                            min(int(self.width/2+self.fuy_width/2),
+                                self.width-2))),
+            (random.randint(max(int(self.height/2-self.fuy_height/2), 1),
+                            min(int(self.height/2+self.fuy_height/2),
+                                self.height-2))))
         choices = []
         for i in range(self.width):
             for j in range(self.height):
-                choices.append((i,j))
-        choices.remove((self.fugitive.x,self.fugitive.y))
-        for l in range(nb_cond):
+                choices.append((i, j))
+        choices.remove((self.fugitive.x, self.fugitive.y))
+        for k in range(nb_cond):
             self.l_cond.append(random.choice(choices))
-            choices.remove(self.l_cond[l])
-            
+            choices.remove(self.l_cond[k])
+
     def __repr__(self):
-        # Characteristics of the board to be displayed:
+        """Return a representation of the board."""
         return(f"Width {self.width}\nHeight {self.height}\nCondemned cells {self.l_cond}")
-        
-    def cond(self,x,y):
-        # Condemn a cell.
-        pos = x,y
-        if (pos not in self.l_cond) and (pos != (self.fugitive.x, self.fugitive.y)):
+
+    def cond(self, x, y):
+        """Condemn a cell."""
+        pos = x, y
+        if ((pos not in self.l_cond)
+                and (pos != (self.fugitive.x, self.fugitive.y))):
             self.l_cond.append(pos)
             return 1
         return 0
 
+
 # Tests for some basic characteristics. More are tested in the controller.
 if __name__ == "__main__":
     # test board conception
-    myBoard = Board(10,5,6,3,3)
-    print(myBoard,"\n")
-    
-    myBoard.cond(0,0)
-    print(myBoard,"\n")
-    
+    myBoard = Board(10, 5, 6, 3, 3)
+    print(myBoard, "\n")
+
+    myBoard.cond(0, 0)
+    print(myBoard, "\n")
+
     # test that fugitive moves
     myFugitive = myBoard.fugitive
     print(myFugitive)
--- CtD_view.py
+++ CtD_view.py
@@ -1,4 +1,21 @@
-import os,sys
+"""
+The view manages the user interface.
+
+Classes:
+
+    Scene
+    View
+    Message box
+    Params
+    Widget
+    Window
+
+Function:
+
+    main
+"""
+
+import os
 
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -8,62 +25,75 @@ from CtD_controller import *
 
 
 class Scene(QGraphicsScene):
+    """Represent the board and the fugitive."""
+
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
         self.controller.add_client(self)
         self.parent = parent
-        self.l = 512;
-        self.setSceneRect(0,0,self.l,self.l)
-        
+        self.l = 512
+        self.setSceneRect(0, 0, self.l, self.l)
+
         # See if packages can be imported to adapt behavior:
         try:
             from tinydb import TinyDB, Query
             self.tinyInitialized = 1
-        except:
+        except Exception:
             self.tinyInitialized = 0
-        
+
     def refresh(self):
-        ## Scene
+        """Update the scene."""
+        # Scene
         if self.controller.valid:
             # Background
             self.clear()
             pen = QPen(Qt.black)
             brush = QBrush(Qt.gray)
-            self.fond = self.addRect(0,0,self.l,self.l,pen,brush)
-            
+            self.fond = self.addRect(0, 0, self.l, self.l, pen, brush)
+
             # Cells
             w = self.controller.myBoard.width
             h = self.controller.myBoard.height
-            self.cellSpace = self.l//(max(w,h)+1)
+            self.cellSpace = self.l//(max(w, h)+1)
             self.cellSize = self.cellSpace - 5
-            self.border = (self.l-self.cellSpace*(max(w,h)))//2
+            self.border = (self.l-self.cellSpace*(max(w, h)))//2
             cells = []
             for j in range(h):
                 for i in range(w):
-                     cells.append(self.addRect(0,0,self.cellSize,self.cellSize,pen,QBrush(Qt.white)))
-                     cells[j*w+i].setPos(self.border+i*self.cellSpace,self.border+(2*j+i%2)*(self.cellSpace//2))
-            
+                    cells.append(
+                        self.addRect(0, 0, self.cellSize, self.cellSize, pen,
+                                     QBrush(Qt.white)))
+                    cells[j*w+i].setPos(
+                        self.border+i*self.cellSpace,
+                        self.border+(2*j+i % 2)*(self.cellSpace//2)
+                        )
+
             # Condemned cells
             l_cond = self.controller.myBoard.l_cond
             cond_cells = []
-            for l,pos in enumerate(l_cond):
+            for k, pos in enumerate(l_cond):
                 i = pos[0]
                 j = pos[1]
-                cond_cells.append(self.addRect(0,0,self.cellSize,self.cellSize,pen,QBrush(Qt.red)))
-                cond_cells[l].setPos(cells[j*w+i].scenePos())
-            
+                cond_cells.append(
+                    self.addRect(0, 0, self.cellSize, self.cellSize,
+                                 pen, QBrush(Qt.red))
+                    )
+                cond_cells[k].setPos(cells[j*w+i].scenePos())
+
             # fugitive
             fugitive = self.controller.myBoard.fugitive
             i = fugitive.x
             j = fugitive.y
             space = 2
-            if i<w and j<h:
-                fugitiveGraphic = self.addEllipse(space,space,self.cellSize-2*space,self.cellSize-2*space,pen,QBrush(Qt.blue))
+            if i < w and j < h:
+                fugitiveGraphic = self.addEllipse(
+                    space, space, self.cellSize-2*space, self.cellSize-2*space,
+                    pen, QBrush(Qt.blue))
                 fugitiveGraphic.setPos(cells[j*w+i].pos())
-            
+
             temp = self.controller.state.split()
-            
+
             # Win
             if temp[0] == 'stuck':
                 if not self.tinyInitialized:
@@ -71,57 +101,62 @@ class Scene(QGraphicsScene):
                 elif self.controller.best_score is None:
                     best = True
                 else:
-                    best = self.controller.nbTurns<self.controller.best_score
+                    best = self.controller.nbTurns < self.controller.best_score
                 self.game_win(best)
-            
+
             # Loose
             elif temp[0] == 'free':
                 self.game_over()
-                
+
         else:
             pass
-        
+
     def mousePressEvent(self, e):
+        """Send user click information to the controller."""
         w = self.controller.w
         h = self.controller.h
         x = e.scenePos().x()
         y = e.scenePos().y()
         try:
             i = int((x-self.border)/self.cellSpace)
-            j = int((y-self.border/2)/self.cellSpace-(i)%2/2) # Hexagonal stuff.
-            if i>=0 and i<w and j >=0 and j<h:
-                self.controller.condemn(i,j)
-        except:
+            j = int((y-self.border/2)/self.cellSpace-(i) % 2/2)     # Hexagonal
+            if i >= 0 and i < w and j >= 0 and j < h:
+                self.controller.condemn(i, j)
+        except Exception:
             pass
-        
-    def game_win(self,best):
+
+    def game_win(self, best):
+        """Display appropriate information when the player wins."""
         self.clear()
         pen = QPen(Qt.black)
         brush = QBrush(Qt.gray)
-        self.fond = self.addRect(0,0,self.l,self.l,pen,brush)
-        font = QFont('Arial',24,QFont.Bold)
+        self.fond = self.addRect(0, 0, self.l, self.l, pen, brush)
+        font = QFont('Arial', 24, QFont.Bold)
         if best:
-            self.gbTxt = self.addText('BEST SCORE !',font)
+            self.gbTxt = self.addText('BEST SCORE !', font)
             self.gbTxt.setDefaultTextColor(Qt.black)
-            self.gbTxt.setPos(6*self.l/20,9*self.l/20)
+            self.gbTxt.setPos(6*self.l/20, 9*self.l/20)
             self.controller.save_score()
         else:
-            self.gwTxt = self.addText('YOU WON !',font)
+            self.gwTxt = self.addText('YOU WON !', font)
             self.gwTxt.setDefaultTextColor(Qt.black)
-            self.gwTxt.setPos(7*self.l/20,9*self.l/20)
-        
-        
+            self.gwTxt.setPos(7*self.l/20, 9*self.l/20)
+
     def game_over(self):
+        """Display appropriate message when player looses."""
         self.clear()
         pen = QPen(Qt.black)
         brush = QBrush(Qt.gray)
-        self.fond = self.addRect(0,0,self.l,self.l,pen,brush)
-        font = QFont('Arial',24,QFont.Bold)
-        self.goTxt = self.addText('GAME OVER !',font)
+        self.fond = self.addRect(0, 0, self.l, self.l, pen, brush)
+        font = QFont('Arial', 24, QFont.Bold)
+        self.goTxt = self.addText('GAME OVER !', font)
         self.goTxt.setDefaultTextColor(Qt.black)
-        self.goTxt.setPos(6*self.l/20,9*self.l/20)
+        self.goTxt.setPos(6*self.l/20, 9*self.l/20)
+
 
 class View(QGraphicsView):
+    """Control the window size."""
+
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
@@ -130,12 +165,17 @@ class View(QGraphicsView):
         self.setScene(self.scene)
 
     def resizeEvent(self, event):
+        """Resize the window."""
         self.fitInView(self.scene.sceneRect(), Qt.KeepAspectRatio)
-    
+
     def refresh(self):
+        """Update the object."""
         pass
 
+
 class message_box(QTextEdit):
+    """Writable box."""
+
     def __init__(self, controller):
         super().__init__()
         self.controller = controller
@@ -143,16 +183,20 @@ class message_box(QTextEdit):
         self.setReadOnly(True)
 
     def refresh(self):
+        """Update the object."""
         message = self.controller.message
         if message:
             self.append(message)
-    
+
+
 class Params(QWidget):
+    """Manages the layout of the parameters (everything but the board)."""
+
     def __init__(self, parent, controller):
         super().__init__()
         self.controller = controller
         self.controller.add_client(self)
-        
+
         # Widgets
         self.start_button = QPushButton('New game')
         self.gridWidth_box = QSpinBox()
@@ -161,37 +205,36 @@ class Params(QWidget):
         self.level_box = QComboBox()
         self.searchPath_button = QPushButton("Browse")
         self.save_button = QPushButton("Save game")
-        self.b_score_lay = QLabel('Best score : '+str(self.controller.best_score))
+        self.b_score_lay = QLabel(f'Best score : {self.controller.best_score}')
         self.score_lay = QLabel('Moves : '+str(self.controller.nbTurns))
-        self.log_box = message_box(self.controller) # Message box
-        
-        
+        self.log_box = message_box(self.controller)     # Message box
+
         # Default value
         self.gridWidth_box.setValue(11)
         self.gridWidth_box.setMinimum(3)
         self.gridHeight_box.setValue(12)
         self.gridHeight_box.setMinimum(3)
         self.gridInit_boxes.setValue(6)
-        self.level_box.addItems(['1 (blind)','2','3'])
+        self.level_box.addItems(['1 (blind)', '2', '3'])
         self.log_box.clear()
         self.log_box.append("Welcome to Circle the dot!\n")
-        
+
         # Slots
         self.searchPath_button.clicked.connect(self.on_load)
         self.start_button.clicked.connect(self.on_start)
         self.save_button.clicked.connect(self.on_save)
         self.level_box.currentTextChanged.connect(self.change_level)
-        
+
         # Layout
         self.formLayout = QFormLayout()
         self.formLayout.addRow('Grid Width', self.gridWidth_box)
         self.formLayout.addRow('Grid Height', self.gridHeight_box)
         self.formLayout.addRow('Initial red boxes', self.gridInit_boxes)
-        self.formLayout.addRow('Fugitive level',self.level_box)
-        
+        self.formLayout.addRow('Fugitive level', self.level_box)
+
         self.formLayout2 = QFormLayout()
-        self.formLayout2.addRow('Load game',self.searchPath_button)
-        
+        self.formLayout2.addRow('Load game', self.searchPath_button)
+
         vLayout = QVBoxLayout()
         vLayout.addLayout(self.formLayout)
         vLayout.addWidget(self.start_button)
@@ -201,50 +244,64 @@ class Params(QWidget):
         vLayout.addWidget(self.b_score_lay)
         vLayout.addWidget(self.score_lay)
         vLayout.addWidget(self.log_box)
-        
+
         self.setLayout(vLayout)
-    
+
     def on_start(self):
+        """Initialise parameters"""
         self.controller.w = self.gridWidth_box.value()
         self.controller.h = self.gridHeight_box.value()
         self.controller.nb_cond = self.gridInit_boxes.value()
         self.controller.start()
-        
+
     def on_load(self):
+        """Update parameters when loading a game."""
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"Load game","","All Files (*);;Python Files (*.py)", options=options)
+        fileName, _ = QFileDialog.getOpenFileName(
+            self, "Load game", "", "All Files (*);;Python Files (*.py)",
+            options=options
+            )
         try:
-            name=os.path.basename(fileName)
+            name = os.path.basename(fileName)
             self.searchPath_button.setText(name)
-        except:
+        except Exception:
             self.log_box.append('There was an error with the file submitted')
             return(0)
         try:
             self.controller.load_game(fileName)
-        except:
-            self.log_box.append("the file submitted does not have the expected layout")
-    
+        except Exception:
+            self.log_box.append(
+                "the file submitted does not have the expected layout")
+
     def on_save(self):
+        """Trigger parameters' backup."""
         try:
-            fileName = QFileDialog.getSaveFileName(self,"Select checkpoint", "","All Files(*)")
+            fileName = QFileDialog.getSaveFileName(self, "Select checkpoint",
+                                                   "", "All Files(*)")
             self.controller.save_game(fileName)
-        except:
-            self.log_box.append("Game could not be saved. Please create a game.\n")
-    
+        except Exception:
+            self.log_box.append(
+                "Game could not be saved. Please create a game.\n")
+
     def change_level(self):
+        """Trigger level change."""
         print("Level changed to", int(self.level_box.currentIndex())+1)
         self.controller.choose_level(int(self.level_box.currentIndex()))
-    
+
     def refresh(self):
+        """Update object."""
         self.gridWidth_box.setValue(self.controller.w)
         self.gridHeight_box.setValue(self.controller.h)
         self.gridInit_boxes.setValue(self.controller.nb_cond)
         self.level_box.setCurrentIndex(self.controller.level)
-        self.b_score_lay.setText('Best score : ' + str(self.controller.best_score))
+        self.b_score_lay.setText(f'Best score : {self.controller.best_score}')
         self.score_lay.setText('Moves : ' + str(self.controller.nbTurns))
-    
+
+
 class Widget(QWidget):
+    """Manages the layout of the objects defined in previous classes."""
+
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
@@ -255,11 +312,15 @@ class Widget(QWidget):
         layout.addWidget(params)
         layout.addWidget(view)
         self.setLayout(layout)
-    
+
     def refresh(self):
+        """Update object."""
         pass
-    
+
+
 class Window(QMainWindow):
+    """Main window: Widget plus title."""
+
     def __init__(self, controller):
         super().__init__()
         self.controller = controller
@@ -269,14 +330,18 @@ class Window(QMainWindow):
         self.setCentralWidget(mainwidget)
 
     def refresh(self):
+        """Update object."""
         pass
-    
+
+
 def main():
+    """Instanciate necessary classes and display application."""
     app = QApplication([])
     controller = CtDController()
     win = Window(controller)
     win.show()
     app.exec()
+
 
 if __name__ == "__main__":
     main()
